482b1be8d4a3ffbe5f3485fc2829195bac148b5e
Douglas.neves <douglas.neves@defensoria.mg.def.br>
2022-05-05 13:38:21 -0300

    ajustes q-card

--- quasar-login-page/src/components/FormsCadastro.vue
+++ quasar-login-page/src/components/FormsCadastro.vue
@@ -1,75 +1,80 @@
 <template>
   <div class="cardOne" style="align-items: center; max-width: 200vh">
-    <q-form
-        class="q-gutter-md"
-    >
-      <q-input rounded
-               v-model="user.get_cadastro.nome"
-               label="Nome Completo*"
-               filled
-               lazy-rules
-               :rules="[ val => val && val.length > 0 || 'Nome Obrigatorio']"/>
-      <q-input filled
-               lazy-rules
-               rounded
-               v-model="user.get_cadastro.cpf"
-               label="CPF*"
-               :rules="[ val => val && val.length > 0 || 'CPF Obrigatorio']"
-               mask="###.###.###-##"/>
-      <q-input filled
-               lazy-rules
-               rounded
-               v-model="user.get_cadastro.rg"
-               label="RG"
-               :rules="[ val => val && val.length > 0 || 'RG Obrigatorio']"
-               mask="##.###.###"/>
-      <q-input filled
-               lazy-rules
-               rounded
-               v-model="user.get_cadastro.email"
-               :rules="[ val => val && val.length > 0 || 'Email Obrigatorio']"
-               label="Email"/>
-      <q-input filled
-               lazy-rules
-               rounded
-               v-model="user.get_cadastro.emailRepet"
-               :rules="[ val => val && val.length > 0 || 'Email Obrigatorio']"
-               label="Digite novamente o Email"/>
-      <q-input filled
-               lazy-rulesr
-               rounded
-               ounded
-               mask="(##) #####-####"
-               v-model="user.get_cadastro.celular"
-               label="Celular*"/>
-      <q-input filled
-               lazy-rules
-               rounded
-               v-model="user.get_cadastro.senhaOne"
-               label="Digite uma senha*"/>
-      <q-input rounded
-               filled
-               lazy-rules
-               v-model="user.get_cadastro.senhaTwo"
-               label="Digite novamente a mesma Senha*"/>
-      <q-toggle v-model="user.get_cadastro.accept"
-                label="Eu aceito os termos*"
-                :value="true"/>
-      <br/>
-      <div>
-        <q-btn icon="fab fa-solid fa-bomb"
-               class="#483D8B text-primary"
-               :disable="!user.get_cadastro.accept"
-               @click.prevent="puxar"
-               label="Avançar"/>
-        <q-btn @click.prevent="puxar"
-               label="Cancelar"
-               type="reset"
-               color="primary"
-               flat
-               class="q-ml-sm"/>
-      </div>
-    </q-form>
+    <q-card>
+      <q-card-section>
+        <q-form
+            class="q-gutter-md"
+        >
+          <q-input rounded
+                   v-model="user.get_cadastro.nome"
+                   label="Nome Completo*"
+                   filled
+                   lazy-rules
+                   :rules="[ val => val && val.length > 0 || 'Nome Obrigatorio']"/>
+          <q-input filled
+                   lazy-rules
+                   rounded
+                   v-model="user.get_cadastro.cpf"
+                   label="CPF*"
+                   :rules="[ val => val && val.length > 0 || 'CPF Obrigatorio']"
+                   mask="###.###.###-##"/>
+          <q-input filled
+                   lazy-rules
+                   rounded
+                   v-model="user.get_cadastro.rg"
+                   label="RG"
+                   :rules="[ val => val && val.length > 0 || 'RG Obrigatorio']"
+                   mask="##.###.###"/>
+          <q-input filled
+                   lazy-rules
+                   rounded
+                   v-model="user.get_cadastro.email"
+                   :rules="[ val => val && val.length > 0 || 'Email Obrigatorio']"
+                   label="Email"/>
+          <q-input filled
+                   lazy-rules
+                   rounded
+                   v-model="user.get_cadastro.emailRepet"
+                   :rules="[ val => val && val.length > 0 || 'Email Obrigatorio']"
+                   label="Digite novamente o Email"/>
+          <q-input filled
+                   lazy-rulesr
+                   rounded
+                   ounded
+                   mask="(##) #####-####"
+                   v-model="user.get_cadastro.celular"
+                   label="Celular*"/>
+          <q-input filled
+                   lazy-rules
+                   rounded
+                   v-model="user.get_cadastro.senhaOne"
+                   label="Digite uma senha*"/>
+          <q-input rounded
+                   filled
+                   lazy-rules
+                   v-model="user.get_cadastro.senhaTwo"
+                   label="Digite novamente a mesma Senha*"/>
+          <q-toggle v-model="user.get_cadastro.accept"
+                    label="Eu aceito os termos*"
+                    :value="true"/>
+        </q-form>
+      </q-card-section>
+      <q-card-actions>
+        <div>
+          <q-btn icon="fab fa-solid fa-bomb"
+                 class="#483D8B text-primary"
+                 :disable="!user.get_cadastro.accept"
+                 @click.prevent="puxar"
+                 label="Avançar"/>
+          <q-btn @click.prevent="puxar"
+                 label="Cancelar"
+                 type="reset"
+                 color="primary"
+                 flat
+                 class="q-ml-sm"/>
+        </div>
+      </q-card-actions>
+    </q-card>
   </div>
 
 </template>
